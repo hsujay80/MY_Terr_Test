5473b6a4af60df9e44dadbb2e5c7ddce7623b579
Sujay <42913490+hsujay80@users.noreply.github.com>
2021-01-24 15:53:53 +0530

    Update Baseec2.tf

--- Baseec2.tf
+++ Baseec2.tf
@@ -8,7 +8,7 @@
   ami                    = "ami-0ebc8f6f580a04647"
   instance_type          = "t2.micro"
   key_name               = "My-key"
-  vpc_security_group_id  = "sg-02e0eb08f5a91c0f6"
+  vpc_security_group_ids  = "sg-02e0eb08f5a91c0f6"
   subnet_id              = "subnet-490da022"
 
   provisioner"local-exec" {
