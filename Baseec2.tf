03d9dfa4d9e2b68c51349c2eda3e80c21c5671ef
Sujay <42913490+hsujay80@users.noreply.github.com>
2021-01-24 15:48:21 +0530

    Update Baseec2.tf

--- Baseec2.tf
+++ Baseec2.tf
@@ -3,16 +3,15 @@
   region = "us-east-2"
 }
  
- resource "My_ec2" {
+ resource "aws_instance" "My Ec2"{
   name                   = "Jenkins"
-  ami                    = "ami-ebd02392"
+  ami                    = "ami-0ebc8f6f580a04647"
   instance_type          = "t2.micro"
   key_name               = "My-key"
-  vpc_security_group_id = "sg-02e0eb08f5a91c0f6"
+  vpc_security_group_id  = "sg-02e0eb08f5a91c0f6"
   subnet_id              = "subnet-490da022"
 
-  provisioner"local-exec"
-  {
+  provisioner"local-exec" {
       command = "sudo apt-get update" 
       command = "sudo apt-get install open-jdk"
       command = "sudo apt-get install jenkins" 
