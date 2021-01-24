b714a68ef48710b6f237136ccf0a690ce6ab36cb
Sujay <42913490+hsujay80@users.noreply.github.com>
2021-01-24 15:52:57 +0530

    Update Baseec2.tf

--- Baseec2.tf
+++ Baseec2.tf
@@ -3,7 +3,7 @@
   region = "us-east-2"
 }
  
- resource "aws_instance" "My Ec2"{
+ resource "aws_instance" "My_Ec2"{
   name                   = "Jenkins"
   ami                    = "ami-0ebc8f6f580a04647"
   instance_type          = "t2.micro"
